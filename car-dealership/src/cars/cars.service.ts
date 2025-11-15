d9e966b3863a2201107fbde44336a58f29b62a29
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-15 13:55:45 -0600

    feat: refactor CarsService to use UUIDs for car IDs and update CarsController to handle string IDs

--- car-dealership/src/cars/cars.service.ts
+++ car-dealership/src/cars/cars.service.ts
@@ -1,20 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 0, make: 'Toyota', model: 'Corolla', year: 2020 },
-    { id: 1, make: 'Honda', model: 'Civic', year: 2019 },
-    { id: 2, make: 'Ford', model: 'Mustang', year: 2021 },
-    { id: 3, make: 'Chevrolet', model: 'Camaro', year: 2018 },
-    { id: 4, make: 'Tesla', model: 'Model 3', year: 2022 },
+  private cars: Car[] = [
+    { id: uuid(), make: 'Toyota', model: 'Corolla', year: 2020 },
+    { id: uuid(), make: 'Honda', model: 'Civic', year: 2019 },
+    { id: uuid(), make: 'Ford', model: 'Mustang', year: 2021 },
+    { id: uuid(), make: 'Chevrolet', model: 'Camaro', year: 2018 },
+    { id: uuid(), make: 'Tesla', model: 'Model 3', year: 2022 },
   ];
 
   public findAll() {
     return this.cars;
   }
 
-  public findById(id: number) {
+  public findById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) {
