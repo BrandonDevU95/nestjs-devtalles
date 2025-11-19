111b5dbfbd0e065424d9fa596cc4c93f904c4fb8
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-18 18:42:40 -0600

    feat: add class-transformer and class-validator dependencies; implement validation in CreateCarDto and CarsController

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -8,6 +8,8 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dtos/create-car.dto';
@@ -27,6 +29,7 @@ export class CarsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createCar(@Body() createCarDto: CreateCarDto) {
     return {
       message: `Create car ${createCarDto.make} ${createCarDto.model}, ${createCarDto.year} - to be implemented`,
--- car-dealership/src/cars/dtos/create-car.dto.ts
+++ car-dealership/src/cars/dtos/create-car.dto.ts
@@ -1,6 +1,12 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateCarDto {
+  @IsString()
   readonly make: string;
+
+  @IsString()
   readonly model: string;
+
+  @IsNumber()
   readonly year: number;
-  readonly price: number;
 }
