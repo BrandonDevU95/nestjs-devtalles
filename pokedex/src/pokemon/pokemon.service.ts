2ecb64d4c86084f74073f4614bcaeb3c50721f56
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-20 18:48:34 -0600

    refactor: enhance error handling in create method of PokemonService

--- pokedex/src/pokemon/pokemon.service.ts
+++ pokedex/src/pokemon/pokemon.service.ts
@@ -1,5 +1,9 @@
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -14,8 +18,20 @@ export class PokemonService {
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    const pokemon = this.PokemonModel.create(createPokemonDto);
-    return pokemon;
+    try {
+      const pokemon = await this.PokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `Pokemon already exists in database: ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+      console.log(error);
+      throw new InternalServerErrorException(
+        'Something went wrong creating the pokemon.',
+      );
+    }
   }
 
   findAll() {
