fd1a0fe7b922f8c9fa8129a378f6f335bc88de17
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-28 00:55:08 -0600

    feat: enhance SeedService to support bulk seeding of Pokemon data

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 
 @Injectable()
 export class SeedService {
@@ -12,17 +13,26 @@ export class SeedService {
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
+
   private readonly axios: AxiosInstance = axios;
 
   async runSeed() {
+    const pokemonToInsert: CreatePokemonDto[] = [];
+
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
     data.results.forEach(async ({ name, url }) => {
       const segment = url.split('/');
       const number = +segment[segment.length - 2];
+
+      pokemonToInsert.push({ name, number });
     });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return 'Seed executed';
   }
 }
