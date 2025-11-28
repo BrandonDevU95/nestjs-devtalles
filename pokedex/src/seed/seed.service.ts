6c14ff9c48d45fe9ad4cc25fd44e3500ea58e846
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-28 01:33:03 -0600

    feat: implement AxiosAdapter for HTTP requests and integrate it into SeedService

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,27 +1,25 @@
-import axios, { AxiosInstance } from 'axios';
-
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { AxiosAdapter } from 'src/common/adapters/axios-adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
-
-  private readonly axios: AxiosInstance = axios;
 
   async runSeed() {
     const pokemonToInsert: CreatePokemonDto[] = [];
 
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
