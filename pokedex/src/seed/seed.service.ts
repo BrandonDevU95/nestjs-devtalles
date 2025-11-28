691a5d0a6c1ad943d0eced3570586e5aa2122254
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-27 18:45:07 -0600

    feat: add axios for API requests and implement PokeResponse interface in SeedService

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,8 +1,21 @@
+import axios, { AxiosInstance } from 'axios';
+
 import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  runSeed() {
-    return 'This action runs the seed';
+  private readonly axios: AxiosInstance = axios;
+
+  async runSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(async ({ name, url }) => {
+      const segment = url.split('/');
+      const number = +segment[segment.length - 2];
+    });
+    return data.results;
   }
 }
