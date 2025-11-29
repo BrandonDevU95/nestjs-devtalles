d10d1996dfee841837b60e110dd364599ca32b88
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-11-29 17:05:57 -0600

    feat: add environment configuration support with EnvConfig and update app.module to load environment variables

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
+import { EnvConfig } from './config/app.config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -9,12 +10,14 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfig],
+    }), // Esta línea carga las variables de entorno desde el archivo .env
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-    }),
+    }), // Sirve archivos estáticos desde la carpeta 'public'
+    MongooseModule.forRoot(process.env.MONGO_URI!), // Conexión a MongoDB usando la variable de entorno
     PokemonModule,
-    MongooseModule.forRoot(process.env.MONGO_URI!),
     CommonModule,
     SeedModule,
   ],
---
+++ pokedex/src/config/app.config.ts
@@ -0,0 +1,8 @@
+import { mongo } from 'mongoose';
+
+export const EnvConfig = () => ({
+  enviroment: process.env.NODE_ENV || 'dev',
+  mongoUri: process.env.MONGO_URI,
+  port: +process.env.PORT! || 3000,
+  defaultLimit: +process.env.DEFAULT_LIMIT! || 10,
+});
