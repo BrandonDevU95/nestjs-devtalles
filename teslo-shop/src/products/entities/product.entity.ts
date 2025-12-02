50b38dd9a1da5d77e81e1ba5522cc4cff3059abf
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-12-02 00:48:36 -0600

    feat: add additional fields to Product entity including price, description, slug, stock, sizes, and gender

--- teslo-shop/src/products/entities/product.entity.ts
+++ teslo-shop/src/products/entities/product.entity.ts
@@ -10,4 +10,34 @@ export class Product {
     nullable: false,
   })
   title: string;
+
+  @Column('float', {
+    default: 0,
+  })
+  price: number;
+
+  @Column('text', {
+    nullable: true,
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+    nullable: false,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+    default: [],
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
 }
