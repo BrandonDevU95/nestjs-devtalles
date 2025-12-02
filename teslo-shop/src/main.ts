23da4e644033e61b39f00c63aafd294cd305bfd8
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-12-02 01:11:27 -0600

    feat: add class-validator decorators to CreateProductDto and implement global validation pipe

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // lanza un error si hay propiedades no definidas en el DTO
+      transform: true, // transforma los payloads a los tipos definidos en los DTOs
+      transformOptions: {
+        exposeUnsetFields: false, // no expone campos no definidos en el DTO
+        enableImplicitConversion: true, // habilita la conversión implícita de tipos
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
