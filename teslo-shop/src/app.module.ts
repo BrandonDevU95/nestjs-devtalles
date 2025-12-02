8fda3f78d51de3a611de623776941aca39f0756d
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-12-02 00:35:24 -0600

    feat: add configuration for PostgreSQL database connection and environment variables

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,7 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT!,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true, // Carga automática de entidades registradas
+      synchronize: process.env.POSTGRES_SYNC === 'true', // Sincroniza el esquema de la base de datos con las entidades (solo en desarrollo)
+    }),
+  ],
   controllers: [],
   providers: [],
 })
