717a1e0e836178d8e50d8766591ff6ea3a81caea
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-12-03 16:12:56 -0600

    feat: update findOne method to use findOnePlain and enhance findAll to return image URLs

--- teslo-shop/src/products/products.service.ts
+++ teslo-shop/src/products/products.service.ts
@@ -42,25 +42,33 @@ export class ProductsService {
     }
   }
 
-  findAll(paginationDto: PaginationDto) {
+  async findAll(paginationDto: PaginationDto) {
     const { limit = 10, offset = 0 } = paginationDto;
-    return this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
+      relations: { images: true },
     });
+
+    return products.map((product) => ({
+      ...product,
+      images: product.images?.map((img) => img.url) || [],
+    }));
   }
 
   async findOne(id: string) {
     let product: Product | null;
+
     if (isUUID(id)) {
       product = await this.productRepository.findOneBy({ id });
     } else {
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('product');
       product = await queryBuilder
         .where('UPPER(title) =:title or slug =:slug', {
           title: id.toUpperCase(),
           slug: id.toLowerCase(),
         })
+        .leftJoinAndSelect('product.images', 'productImages')
         .getOne();
     }
 
@@ -68,6 +76,14 @@ export class ProductsService {
       throw new NotFoundException(`Product with id or slug ${id} not found`);
 
     return product;
+  }
+
+  async findOnePlain(id: string) {
+    const { images = [], ...product } = await this.findOne(id);
+    return {
+      ...product,
+      images: images?.map((img) => img.url) || [],
+    };
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
