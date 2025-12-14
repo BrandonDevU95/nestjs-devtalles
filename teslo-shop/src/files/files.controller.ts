e408b5d39bfa75364b50c32fc746b6bed10ad2d2
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-12-14 15:33:01 -0600

    feat: add file upload functionality with file filtering in FilesController

--- teslo-shop/src/files/files.controller.ts
+++ teslo-shop/src/files/files.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -7,14 +8,23 @@ import {
 
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
+import { fileFilter } from './helpers/file-filter';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('File is empty or unsupported file type');
+    }
+
     return file;
   }
 }
