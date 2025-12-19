4fa4ef2c62d33299e1aa4e6e85cde7879fcf1e55
BrandonDevU <brandon.vargas1095@hotmail.com>
2025-12-18 19:26:12 -0600

    feat: add JwtStrategy for JWT authentication and update AuthModule exports

--- teslo-shop/src/auth/strategies/jwt.strategy.ts
+++ teslo-shop/src/auth/strategies/jwt.strategy.ts
@@ -1,14 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get<string>('JWT_SECRET') || '',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
   async validate(payload: JwtPayload): Promise<User> {
     const { email } = payload;
 
-    const user = new User();
-    user.email = email;
+    const user = await this.userRepository.findOneBy({ email });
+    if (!user) {
+      throw new UnauthorizedException('Token not valid');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('User is inactive');
+    }
+
     return user;
   }
 }
