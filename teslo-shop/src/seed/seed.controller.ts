c5cbdf1630dc214dbdbc4137298197f1f0be7ce0
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2026-05-21 17:24:37 -0600

    feat: add Auth module import and apply Auth decorator to SeedController for role-based access control

--- teslo-shop/src/seed/seed.controller.ts
+++ teslo-shop/src/seed/seed.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { Auth } from 'src/auth/decorators';
 import { SeedService } from './seed.service';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  @Auth(ValidRoles.superUser)
   executeSeeds() {
     return this.seedService.runSeed();
   }
