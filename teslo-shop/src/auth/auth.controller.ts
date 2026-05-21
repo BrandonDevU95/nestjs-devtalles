9a100f3647f019c662ab5f4fb9d6abe2112771ed
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2026-05-21 17:16:13 -0600

    feat: add Auth decorator to combine RoleProtected and UseGuards for simplified route protection

--- teslo-shop/src/auth/auth.controller.ts
+++ teslo-shop/src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
-import { GetUser, RoleProtected, RowHeaders } from './decorators';
+import { Auth, GetUser, RoleProtected, RowHeaders } from './decorators';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
 import { ValidRoles } from './interfaces';
 
@@ -80,6 +80,17 @@ export class AuthController {
       user,
       userEmail,
       headers,
+    };
+  }
+
+  @Get('private4')
+  //Implementa el decorador Auth() que combina el decorador de RoleProtected y el de UseGuards, de esta forma se puede usar el decorador Auth() en lugar de tener que usar ambos decoradores por separado
+  @Auth(ValidRoles.admin, ValidRoles.superUser)
+  testingPrivateRoute4(@GetUser() user: User) {
+    return {
+      ok: true,
+      message: 'This is another private route',
+      user,
     };
   }
 }
--- teslo-shop/src/auth/decorators/index.ts
+++ teslo-shop/src/auth/decorators/index.ts
@@ -1,3 +1,4 @@
 export { GetUser } from './get-user.decorator';
 export { RowHeaders } from './row-headers.decorator';
 export { RoleProtected } from './role-protected.decorator';
+export { Auth } from './auth.decorator';
